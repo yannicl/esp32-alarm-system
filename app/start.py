532ec4c1e4995feb194fa98b862b7066ea528db1
Yannic Luyckx <yannic.luyckx@gmail.com>
2023-01-20 17:18:16 -0500

    add udp broadcast

--- app/start.py
+++ app/start.py
@@ -27,8 +27,12 @@ class RealBell(Bell):
         self.bellPin.off()
 
 class MqttAlarmStateProcessor(AlarmStateProcessor):
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
+    
     def __init__(self, bell):
         self.mqttClient = None
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         super().__init__(bell)
 
     def setMqttClient(self, client):
@@ -37,12 +41,14 @@ class MqttAlarmStateProcessor(AlarmStateProcessor):
     def publishAlarm(self):
         message = ujson.dumps(self.alarmStatus.export())
         print("Publishing alarm message "+str(message))
+        self.sock.sendto(message.encode('utf-8'), ("255.255.255.255", 5935))
         mqtt_topic = '/devices/{}/{}'.format(config.google_cloud_config['device_id'], 'events')
         if (self.mqttClient is not None):
             self.mqttClient.publish(mqtt_topic.encode('utf-8'), message.encode('utf-8'))
     def publishState(self):
         message = ujson.dumps(self.alarmStatus.export())
         print("Publishing state message "+str(message))
+        self.sock.sendto(message.encode('utf-8'), ("255.255.255.255", 5935))
         mqtt_topic = '/devices/{}/{}'.format(config.google_cloud_config['device_id'], 'state')
         if (self.mqttClient is not None):
             self.mqttClient.publish(mqtt_topic.encode('utf-8'), message.encode('utf-8'))
